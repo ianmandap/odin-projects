4152ffb6d4b5c0386b3025e53eacf3d346d932a2
theIanMilan <mandap.fmg@gmail.com>
2025-01-09 23:19:07 +0800

    Add #insert, #delete, and traversal order methods

--- Ruby Binary Search Trees/lib/tree.rb
+++ Ruby Binary Search Trees/lib/tree.rb
@@ -9,7 +9,7 @@ class Tree
 
   def initialize(array)
     @data = array
-    @root = build_tree(array.sort.uniq)
+    @root = build_tree(data.sort.uniq)
   end
 
   def build_tree(arr, start = 0, last = arr.length - 1)
@@ -24,13 +24,96 @@ class Tree
     root_node
   end
 
-  def insert; end
-  def delete; end
+  def insert(val, root = @root)
+    return Node.new(val) if root.nil?
+
+    if val == root.data
+      return root
+    elsif val < root.data
+      root.left = insert(val, root.left)
+    else
+      root.right = insert(val, root.right)
+    end
+
+    root
+  end
+
+  def delete(val, root = @root)
+    return root if root.nil?
+
+    # Traverse subtrees
+    if val < root.data
+      root.left = delete(val, root.left)
+    elsif val > root.data
+      root.right = delete(val, root.right)
+    else
+      # root matches with val
+      # Case 1: root has no children or only right child
+      return root.right if root.left.nil?
+
+      # Case 2: root only has left child
+      return root.left if root.right.nil?
+
+      # Case 3: both children present
+      successor = get_successor(root)
+      root.data = successor.data
+      root.right = delete(successor.data, root.right)
+    end
+
+    root
+  end
+
   def find; end
-  def level_order; end
-  def preorder; end
-  def inorder; end
-  def postorder; end
+
+  def level_order(node = root)
+    return if node.nil?
+
+    arr = [node.data]
+    queue = [node.left, node.right].compact
+
+    until queue.empty?
+      n = queue.shift
+      arr << n.data
+      queue << n.left unless n.left.nil?
+      queue << n.right unless n.right.nil?
+    end
+
+    arr
+  end
+
+  # root-left-right
+  def preorder(node = root, result = [])
+    return if node.nil?
+
+    result << node.data
+    preorder(node.left, result)
+    preorder(node.right, result)
+
+    result
+  end
+
+  # left-root-right
+  def inorder(node = root, result = [])
+    return if node.nil?
+
+    inorder(node.left, result)
+    result << node.data
+    inorder(node.right, result)
+
+    result
+  end
+
+  # left-right-root
+  def postorder(node = root, result = [])
+    return if node.nil?
+
+    postorder(node.left, result)
+    postorder(node.right, result)
+    result << node.data
+
+    result
+  end
+
   def height; end
   def depth; end
   def balanced?; end
@@ -40,5 +123,17 @@ class Tree
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private
+
+  # Utility function for deleting a node with 2 children
+  # Traverse node's right subtree, and look for farthest left value
+  def get_successor(current)
+    current = current.right
+
+    current = current.left until current.left.nil?
+
+    current
   end
 end
--- Ruby Binary Search Trees/spec/tree_spec.rb
+++ Ruby Binary Search Trees/spec/tree_spec.rb
@@ -3,10 +3,11 @@
 require './lib/tree'
 
 describe Tree do
-  let(:array1) { [1, 2, 3] }
+  let(:array1) { [1, 2, 3, 4] }
   let(:tree1) { Tree.new(array1) }
   let(:array2) { [1, 2, 3, 4, 5, 6, 7] }
   let(:tree2) { Tree.new(array2) }
+  let!(:out) { $stdout = StringIO.new }
 
   describe '#build_tree' do
     it 'builds a balanced tree and returns the level-0 root node' do
@@ -17,17 +18,108 @@ describe Tree do
 
   describe '#pretty_print' do
     it 'pretty prints a balanced tree to stdout' do
-      $stdout = StringIO.new
       tree2.pretty_print
-      $stdout.rewind
+      out.rewind
 
-      expect($stdout.gets).to eq("│       ┌── 7\n")
-      expect($stdout.gets).to eq("│   ┌── 6\n")
-      expect($stdout.gets).to eq("│   │   └── 5\n")
-      expect($stdout.gets).to eq("└── 4\n")
-      expect($stdout.gets).to eq("    │   ┌── 3\n")
-      expect($stdout.gets).to eq("    └── 2\n")
-      expect($stdout.gets).to eq("        └── 1\n")
+      expect(out.gets).to eq("│       ┌── 7\n")
+      expect(out.gets).to eq("│   ┌── 6\n")
+      expect(out.gets).to eq("│   │   └── 5\n")
+      expect(out.gets).to eq("└── 4\n")
+      expect(out.gets).to eq("    │   ┌── 3\n")
+      expect(out.gets).to eq("    └── 2\n")
+      expect(out.gets).to eq("        └── 1\n")
+    end
+  end
+
+  describe '#insert' do
+    it 'inserts node and maintains BST structure' do
+      tree2.insert(4.5)
+      tree2.pretty_print
+      out.rewind
+
+      expect(out.gets).to eq("│       ┌── 7\n")
+      expect(out.gets).to eq("│   ┌── 6\n")
+      expect(out.gets).to eq("│   │   └── 5\n")
+      expect(out.gets).to eq("│   │       └── 4.5\n")
+      expect(out.gets).to eq("└── 4\n")
+      expect(out.gets).to eq("    │   ┌── 3\n")
+      expect(out.gets).to eq("    └── 2\n")
+      expect(out.gets).to eq("        └── 1\n")
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes leaf node and maintains BST structure' do
+      tree2.insert(4.5)
+      tree2.delete(7)
+      tree2.pretty_print
+      out.rewind
+
+      expect(out.gets).to eq("│   ┌── 6\n")
+      expect(out.gets).to eq("│   │   └── 5\n")
+      expect(out.gets).to eq("│   │       └── 4.5\n")
+      expect(out.gets).to eq("└── 4\n")
+      expect(out.gets).to eq("    │   ┌── 3\n")
+      expect(out.gets).to eq("    └── 2\n")
+      expect(out.gets).to eq("        └── 1\n")
+    end
+
+    it 'swaps node if it has one left child and maintains BST structure' do
+      tree2.insert(4.5)
+      tree2.delete(5)
+      tree2.pretty_print
+      out.rewind
+
+      expect(out.gets).to eq("│       ┌── 7\n")
+      expect(out.gets).to eq("│   ┌── 6\n")
+      expect(out.gets).to eq("│   │   └── 4.5\n")
+      expect(out.gets).to eq("└── 4\n")
+      expect(out.gets).to eq("    │   ┌── 3\n")
+      expect(out.gets).to eq("    └── 2\n")
+      expect(out.gets).to eq("        └── 1\n")
+    end
+
+    it 'swaps node with next value if it has one 2 child and maintains BST structure' do
+      tree2.insert(4.5)
+      tree2.delete(4)
+      tree2.pretty_print
+      out.rewind
+
+      expect(out.gets).to eq("│       ┌── 7\n")
+      expect(out.gets).to eq("│   ┌── 6\n")
+      expect(out.gets).to eq("│   │   └── 5\n")
+      expect(out.gets).to eq("└── 4.5\n")
+      expect(out.gets).to eq("    │   ┌── 3\n")
+      expect(out.gets).to eq("    └── 2\n")
+      expect(out.gets).to eq("        └── 1\n")
+    end
+
+    describe '#level_order' do
+      it 'returns array of nodes in level order of BST' do
+        expect(tree1.level_order(tree1.root)).to eql([2, 1, 3, 4])
+        expect(tree2.level_order(tree2.root)).to eql([4, 2, 6, 1, 3, 5, 7])
+      end
+    end
+
+    describe '#preorder' do
+      it 'returns array of nodes in preorder of BST' do
+        expect(tree1.preorder(tree1.root)).to eql([2, 1, 3, 4])
+        expect(tree2.preorder(tree2.root)).to eql([4, 2, 1, 3, 6, 5, 7])
+      end
+    end
+
+    describe '#inorder' do
+      it 'returns array of nodes in inorder of BST' do
+        expect(tree1.inorder(tree1.root)).to eql(array1)
+        expect(tree2.inorder(tree2.root)).to eql(array2)
+      end
+    end
+
+    describe '#postorder' do
+      it 'returns array of nodes in postorder of BST' do
+        expect(tree1.postorder(tree1.root)).to eql([1, 4, 3, 2])
+        expect(tree2.postorder(tree2.root)).to eql([1, 3, 2, 5, 7, 6, 4])
+      end
     end
   end
 end
